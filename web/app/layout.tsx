c278ce904ae7bb62eb04f8df3088aa8cca04ce16
dimka90 <dimkayilrit@gmail.com>
2025-12-26 06:34:20 +0100

    refactor: improve layout theme integration (iteration 8)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -38,3 +38,4 @@ export default function RootLayout({
 // Layout integration improvement 5
 // Layout integration improvement 6
 // Layout integration improvement 7
+// Layout integration improvement 8
