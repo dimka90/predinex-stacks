dcd81c62018ba9a00b48552a078cb3d40b650933
dimka90 <dimkayilrit@gmail.com>
2025-12-28 11:41:07 +0100

    feat: add AppKit constants (13/30)

---
+++ web/lib/appkit-constants.ts
@@ -0,0 +1,7 @@
+export const SUPPORTED_NETWORKS = {
+  MAINNET: 'stacks:mainnet',
+  TESTNET: 'stacks:testnet',
+} as const;
+
+export const DEFAULT_NETWORK = SUPPORTED_NETWORKS.MAINNET;
+export const WALLET_CONNECT_VERSION = '2.0';
