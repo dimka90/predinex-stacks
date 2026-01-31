79816298aa277ae64875c609cf4b08e62247de25
dimka90 <dimkayilrit@gmail.com>
2026-01-31 05:19:56 +0100

    ui(components): add Spinner utility

---
+++ web/components/ui/spinner.tsx
@@ -0,0 +1,5 @@
+export function Spinner({ className }: { className?: string }) {
+    return (
+        <div className={`animate-spin rounded-full h-4 w-4 border-b-2 border-primary ${className}`} />
+    );
+}
