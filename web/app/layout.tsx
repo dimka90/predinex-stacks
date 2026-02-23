a62fb0340170233ac3468b4808d97be137be59d6
dimka90 <dimkayilrit@gmail.com>
2026-02-23 07:29:31 +0100

    feat(layout): integrate ToastProvider into root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { StacksProvider } from "./components/StacksProvider";
+import { ToastProvider } from "../providers/ToastProvider";
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
@@ -52,8 +53,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <StacksProvider>
-          {children}
-          <Footer />
+          <ToastProvider>
+            {children}
+            <Footer />
+          </ToastProvider>
         </StacksProvider>
       </body>
     </html>
