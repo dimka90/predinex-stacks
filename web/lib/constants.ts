953d61df84cc4cead94fad184ce569c56547a7c7
dimka90 <dimkayilrit@gmail.com>
2026-02-23 07:14:37 +0100

    feat: implement multi-admin support, toast notifications, and standardize contract references

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,2 +1,5 @@
 export const APP_NAME = "Predinex";
 export const REWARDS_VERSION = "v1.0";
+
+export const CONTRACT_ADDRESS = "SP2W_EJMBN";
+export const CONTRACT_NAME = "predinex-pool-1771470759824";
