2e0f58c74037138b18a25d259f8058cb820bead0
Predinex Dev <dimka@predinex.io>
2026-03-28 10:43:09 +0100

    feat(hooks): add centralized hooks index for cleaner protocol imports

---
+++ web/app/lib/hooks/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Hooks Index - Centralized exports for all custom React hooks.
+ */
+
+export * from './useLocalStorage';
+export * from './useDebounce';
+export * from './useMediaQuery';
+export * from './useCountdown';
+export * from './useOnClickOutside';
+export * from './useMarketStatus';
+export * from './useWalletConnect';
