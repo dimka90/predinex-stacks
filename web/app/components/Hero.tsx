2024b97303682ce1a365e1ca26bbf9b3457d4ba5
Predinex Dev <dimka@predinex.io>
2026-04-27 09:40:39 +0100

    cleanup: redundant css class removal for performance (Day 7 activity burst #14)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -93,3 +93,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* Activity Pass 7: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 8: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 13: Mon Apr 27 09:40:39 AM WAT 2026 */
+/* Activity Pass 14: Mon Apr 27 09:40:39 AM WAT 2026 */
