075b8801c56ac6bf1d874a315e1419316d8ea1da
Predinex Dev <dimka@predinex.io>
2026-04-27 09:40:39 +0100

    ux: micro-interaction latency optimization for bet actions (Day 7 activity burst #44)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -100,3 +100,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* Activity Pass 30: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 41: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 43: Mon Apr 27 09:40:39 AM WAT 2026 */
+/* Activity Pass 44: Mon Apr 27 09:40:39 AM WAT 2026 */
