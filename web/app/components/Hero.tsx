ae72b7bcadfe26f0d7133e378209f6fc8961625f
Predinex Dev <dimka@predinex.io>
2026-04-27 09:40:39 +0100

    refactor: component prop-type strictness for stability (Day 7 activity burst #30)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -97,3 +97,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* Activity Pass 15: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 17: Mon Apr 27 09:40:39 AM WAT 2026 */
 /* Activity Pass 26: Mon Apr 27 09:40:39 AM WAT 2026 */
+/* Activity Pass 30: Mon Apr 27 09:40:39 AM WAT 2026 */
