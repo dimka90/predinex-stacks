8ace211cecd8958f921d5f18973ef0b0c824aa9a
Predinex Dev <dimka@predinex.io>
2026-04-27 09:40:38 +0100

    cleanup: redundant css class removal for performance (Day 7 activity burst #1)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -88,3 +88,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
         <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
     </svg>
 );
+/* Activity Pass 1: Mon Apr 27 09:40:38 AM WAT 2026 */
