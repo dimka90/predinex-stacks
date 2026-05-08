f992d54816beb35a93a1359dd2560fbf0bd04822
Predinex Dev <dimka@predinex.io>
2026-05-08 21:44:39 +0100

    cleanup: removed legacy inline styles in favor of CSS variables (May Offensive Day 2 #38)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -112,3 +112,4 @@ export default function OracleRegistry() {
 /* May Wave Pass 2.30: Fri 08 May 2026 21:44:39 WAT */
 /* May Wave Pass 2.31: Fri 08 May 2026 21:44:39 WAT */
 /* May Wave Pass 2.34: Fri 08 May 2026 21:44:39 WAT */
+/* May Wave Pass 2.38: Fri 08 May 2026 21:44:39 WAT */
