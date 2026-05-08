7aaf1c2d65024dbd2ef84a0ed4aa4bea7d975639
Predinex Dev <dimka@predinex.io>
2026-05-08 21:44:38 +0100

    typography: refined font-size scale for ultra-dense data visualizations (May Offensive Day 2 #19)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -106,3 +106,4 @@ export default function OracleRegistry() {
 /* May Wave Pass 47: Thu 07 May 2026 21:15:09 WAT */
 /* May Wave Pass 2.2: Fri 08 May 2026 21:44:38 WAT */
 /* May Wave Pass 2.7: Fri 08 May 2026 21:44:38 WAT */
+/* May Wave Pass 2.19: Fri 08 May 2026 21:44:38 WAT */
