1ffd9d6bddb7a9c186fcd6b1f2151ced476fc50a
Predinex Dev <dimka@predinex.io>
2026-05-10 09:12:17 +0100

    polish: refined ambient-light dither-radius for deep background layers (May hyper-offensive #54)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -132,3 +132,4 @@ export default function OracleRegistry() {
 /* UNLEASHED PASS 43: Sun 10 May 2026 09:12:17 WAT */
 /* UNLEASHED PASS 45: Sun 10 May 2026 09:12:17 WAT */
 /* UNLEASHED PASS 52: Sun 10 May 2026 09:12:17 WAT */
+/* UNLEASHED PASS 54: Sun 10 May 2026 09:12:17 WAT */
