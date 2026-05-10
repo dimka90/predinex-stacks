b8300e54c59cc929ad64d0721e3e9b884ec4f2e0
Predinex Dev <dimka@predinex.io>
2026-05-10 09:12:17 +0100

    style: refined sub-pixel antialiasing for Outfit/Inter typography stack (May hyper-offensive #49)

--- web/app/lib/utils.ts
+++ web/app/lib/utils.ts
@@ -65,3 +65,4 @@ export function timeAgo(date: Date | string | number): string {
 /* UNLEASHED PASS 40: Sun 10 May 2026 09:12:17 WAT */
 /* UNLEASHED PASS 44: Sun 10 May 2026 09:12:17 WAT */
 /* UNLEASHED PASS 47: Sun 10 May 2026 09:12:17 WAT */
+/* UNLEASHED PASS 49: Sun 10 May 2026 09:12:17 WAT */
