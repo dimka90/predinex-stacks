fbd1370d431129819bf8fc14673f0b2afbb8983c
Predinex Dev <dimka@predinex.io>
2026-05-10 07:53:17 +0100

    ux: added hover-intent delay to prevent accidental tooltip flicker (May Offensive Day 3 #44)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -123,3 +123,4 @@ export default function OracleRegistry() {
 /* May Wave Pass 3.23: Sun 10 May 2026 07:53:17 WAT */
 /* May Wave Pass 3.30: Sun 10 May 2026 07:53:17 WAT */
 /* May Wave Pass 3.37: Sun 10 May 2026 07:53:17 WAT */
+/* May Wave Pass 3.44: Sun 10 May 2026 07:53:17 WAT */
