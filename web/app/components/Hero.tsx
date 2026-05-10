edc603630e15f60012a96894dd7aee045c2e1c0b
Predinex Dev <dimka@predinex.io>
2026-05-10 09:12:16 +0100

    typography: optimized font-weight consistency for Bloomberg-style data views (May hyper-offensive #19)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -161,3 +161,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* UNLEASHED PASS 13: Sun 10 May 2026 09:12:16 WAT */
 /* UNLEASHED PASS 16: Sun 10 May 2026 09:12:16 WAT */
 /* UNLEASHED PASS 17: Sun 10 May 2026 09:12:16 WAT */
+/* UNLEASHED PASS 19: Sun 10 May 2026 09:12:16 WAT */
