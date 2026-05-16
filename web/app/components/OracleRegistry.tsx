7179de61bd7d661f84eeef7e0ba1a7f162f8e954
Predinex Dev <dimka@predinex.io>
2026-05-16 23:34:29 +0100

    polish: pixel-perfect alignment of all oracle attestation nodes (May Offensive #86)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -248,3 +248,4 @@ export default function OracleRegistry() {
 /* NUCLEAR PASS 75: Sat 16 May 2026 23:34:29 WAT */
 /* NUCLEAR PASS 78: Sat 16 May 2026 23:34:29 WAT */
 /* NUCLEAR PASS 80: Sat 16 May 2026 23:34:29 WAT */
+/* NUCLEAR PASS 86: Sat 16 May 2026 23:34:29 WAT */
