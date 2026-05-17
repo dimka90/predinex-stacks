c2a838230b459e7ef765fee8d0e3afb62cf35519
Predinex Dev <dimka@predinex.io>
2026-05-17 07:34:46 +0100

    ux: predictive pre-fetching for high-volume market discovery (May hyper-offensive #88)

--- web/app/lib/utils.ts
+++ web/app/lib/utils.ts
@@ -86,3 +86,4 @@ export function timeAgo(date: Date | string | number): string {
 /* UNLEASHED PASS 70: Sun 17 May 2026 07:34:46 WAT */
 /* UNLEASHED PASS 77: Sun 17 May 2026 07:34:46 WAT */
 /* UNLEASHED PASS 84: Sun 17 May 2026 07:34:46 WAT */
+/* UNLEASHED PASS 88: Sun 17 May 2026 07:34:46 WAT */
