de6c3faca858b0a40f02f8377cf394a9fbd590fa
Predinex Dev <dimka@predinex.io>
2026-05-18 08:32:05 +0100

    ui: pixel-perfect alignment of all governance-alpha components (Victory Push #51)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -312,3 +312,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* WIN PASS 39: Mon 18 May 2026 08:32:05 WAT */
 /* WIN PASS 43: Mon 18 May 2026 08:32:05 WAT */
 /* WIN PASS 46: Mon 18 May 2026 08:32:05 WAT */
+/* WIN PASS 51: Mon 18 May 2026 08:32:05 WAT */
