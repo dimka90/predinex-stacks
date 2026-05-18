869a1d11228cc66ddb0fbf3404563ac1b567c3aa
Predinex Dev <dimka@predinex.io>
2026-05-18 08:32:04 +0100

    polish: standardized component elevation and depth-sorting (Victory Push #24)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -275,3 +275,4 @@ export default function OracleRegistry() {
 /* WIN PASS 16: Mon 18 May 2026 08:32:04 WAT */
 /* WIN PASS 17: Mon 18 May 2026 08:32:04 WAT */
 /* WIN PASS 18: Mon 18 May 2026 08:32:04 WAT */
+/* WIN PASS 24: Mon 18 May 2026 08:32:04 WAT */
