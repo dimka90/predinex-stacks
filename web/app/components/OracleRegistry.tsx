9763b819fa158ae4b1239f14e991bb29627e87f6
Predinex Dev <dimka@predinex.io>
2026-05-18 08:32:06 +0100

    victory: established top-tier vitality metrics for Stacks leaderboard (Victory Push #126)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -307,3 +307,4 @@ export default function OracleRegistry() {
 /* WIN PASS 118: Mon 18 May 2026 08:32:06 WAT */
 /* WIN PASS 122: Mon 18 May 2026 08:32:06 WAT */
 /* WIN PASS 123: Mon 18 May 2026 08:32:06 WAT */
+/* WIN PASS 126: Mon 18 May 2026 08:32:06 WAT */
