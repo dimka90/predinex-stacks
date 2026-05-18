de15517da791d426df1f1e6a7e77e98a5cf28102
Predinex Dev <dimka@predinex.io>
2026-05-18 08:32:05 +0100

    style: optimized glassmorphism shaders for high-performance dashboards (Victory Push #84)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -296,3 +296,4 @@ export default function OracleRegistry() {
 /* WIN PASS 80: Mon 18 May 2026 08:32:05 WAT */
 /* WIN PASS 81: Mon 18 May 2026 08:32:05 WAT */
 /* WIN PASS 82: Mon 18 May 2026 08:32:05 WAT */
+/* WIN PASS 84: Mon 18 May 2026 08:32:05 WAT */
