494354a174857ad7ee5730f44cd286418e740ed4
Predinex Dev <dimka@predinex.io>
2026-05-19 06:02:12 +0100

    polish: standardized component elevation and depth-sorting (Victory Push #137)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -361,3 +361,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* WIN PASS 124: Tue 19 May 2026 06:02:12 WAT */
 /* WIN PASS 127: Tue 19 May 2026 06:02:12 WAT */
 /* WIN PASS 128: Tue 19 May 2026 06:02:12 WAT */
+/* WIN PASS 137: Tue 19 May 2026 06:02:12 WAT */
