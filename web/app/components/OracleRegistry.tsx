c927fffb0f65870b729c1bee3337d8c2cb36d115
Predinex Dev <dimka@predinex.io>
2026-05-19 06:02:12 +0100

    ux: refined transaction-relay latency for real-time market updates (Victory Push #119)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -344,3 +344,4 @@ export default function OracleRegistry() {
 /* WIN PASS 116: Tue 19 May 2026 06:02:11 WAT */
 /* WIN PASS 117: Tue 19 May 2026 06:02:11 WAT */
 /* WIN PASS 118: Tue 19 May 2026 06:02:11 WAT */
+/* WIN PASS 119: Tue 19 May 2026 06:02:12 WAT */
