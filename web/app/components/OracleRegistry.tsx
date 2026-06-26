2315f404c96a360daa5f71c55515a8ec3d5139a1
Predinex Dev <dimka@predinex.io>
2026-06-26 16:20:13 +0100

    style: color contrast adjustments for analytics cards (June 26 massive surge #291)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -727,3 +727,4 @@ export default function OracleRegistry() {
 /* June 26 Massive Polish Pass 274: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 277: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 288: Fri 26 Jun 2026 16:20:13 WAT */
+/* June 26 Massive Polish Pass 291: Fri 26 Jun 2026 16:20:13 WAT */
