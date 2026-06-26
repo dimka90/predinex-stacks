d9dd16212cecc712dc9ba8965b209fd4489edde5
Predinex Dev <dimka@predinex.io>
2026-06-26 16:20:12 +0100

    analytics: streamlined logging payload for block transition latency (June 26 massive surge #180)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -705,3 +705,4 @@ export default function OracleRegistry() {
 /* June 26 Massive Polish Pass 171: Fri 26 Jun 2026 16:20:12 WAT */
 /* June 26 Massive Polish Pass 172: Fri 26 Jun 2026 16:20:12 WAT */
 /* June 26 Massive Polish Pass 178: Fri 26 Jun 2026 16:20:12 WAT */
+/* June 26 Massive Polish Pass 180: Fri 26 Jun 2026 16:20:12 WAT */
