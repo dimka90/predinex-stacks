3f754973e88ac1a60a7812a61ed70ce4a1c94fd4
Predinex Dev <dimka@predinex.io>
2026-06-26 16:20:13 +0100

    polish: shadow-elevation sync across dropdown menu items (June 26 massive surge #293)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -728,3 +728,4 @@ export default function OracleRegistry() {
 /* June 26 Massive Polish Pass 277: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 288: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 291: Fri 26 Jun 2026 16:20:13 WAT */
+/* June 26 Massive Polish Pass 293: Fri 26 Jun 2026 16:20:13 WAT */
