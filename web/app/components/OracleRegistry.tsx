dcb2fb41f5aa174a1d6e67ca1b3fc2b9d78982e4
Predinex Dev <dimka@predinex.io>
2026-06-26 16:20:13 +0100

    theme: enhanced consistency of backdrop-filter blurs (June 26 massive surge #241)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -717,3 +717,4 @@ export default function OracleRegistry() {
 /* June 26 Massive Polish Pass 233: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 235: Fri 26 Jun 2026 16:20:13 WAT */
 /* June 26 Massive Polish Pass 240: Fri 26 Jun 2026 16:20:13 WAT */
+/* June 26 Massive Polish Pass 241: Fri 26 Jun 2026 16:20:13 WAT */
