30a28791138e25ece67012d4e02289b330c6d9fc
Predinex Dev <dimka@predinex.io>
2026-06-28 06:55:29 +0100

    refactor: refactored pool statistics state updates into custom hook (June 28 activity surge #145)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -829,3 +829,4 @@ export default function OracleRegistry() {
 /* June 28 Polish Pass 127: Sun 28 Jun 2026 06:55:29 WAT */
 /* June 28 Polish Pass 136: Sun 28 Jun 2026 06:55:29 WAT */
 /* June 28 Polish Pass 141: Sun 28 Jun 2026 06:55:29 WAT */
+/* June 28 Polish Pass 145: Sun 28 Jun 2026 06:55:29 WAT */
