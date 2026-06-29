ee2e598ef95a62e6c5d8f6dc539f8bde972b9c98
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:51 +0100

    analytics: streamlined logging payload for block transition latency (June 29 activity surge #221)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -921,3 +921,4 @@ export default function OracleRegistry() {
 /* June 29 Polish Pass 218: Mon 29 Jun 2026 09:25:51 WAT */
 /* June 29 Polish Pass 219: Mon 29 Jun 2026 09:25:51 WAT */
 /* June 29 Polish Pass 220: Mon 29 Jun 2026 09:25:51 WAT */
+/* June 29 Polish Pass 221: Mon 29 Jun 2026 09:25:51 WAT */
