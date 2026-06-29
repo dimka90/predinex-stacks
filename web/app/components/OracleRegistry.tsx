72c5e1d2cfc4174623d0ea487fba9df9adce8459
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:49 +0100

    style: color contrast adjustments for analytics cards (June 29 activity surge #90)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -893,3 +893,4 @@ export default function OracleRegistry() {
 /* June 29 Polish Pass 80: Mon 29 Jun 2026 09:25:49 WAT */
 /* June 29 Polish Pass 83: Mon 29 Jun 2026 09:25:49 WAT */
 /* June 29 Polish Pass 86: Mon 29 Jun 2026 09:25:49 WAT */
+/* June 29 Polish Pass 90: Mon 29 Jun 2026 09:25:49 WAT */
