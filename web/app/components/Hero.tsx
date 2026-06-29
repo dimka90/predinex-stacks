a3f9b89341f81e1926c134329c6e34290c45e4c1
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:52 +0100

    cleanup: deleted obsolete CSS variables and comments (June 29 activity surge #241)

--- web/app/components/Hero.tsx
+++ web/app/components/Hero.tsx
@@ -963,3 +963,4 @@ const Globe = ({ size, className }: { size: number, className?: string }) => (
 /* June 29 Polish Pass 217: Mon 29 Jun 2026 09:25:51 WAT */
 /* June 29 Polish Pass 223: Mon 29 Jun 2026 09:25:51 WAT */
 /* June 29 Polish Pass 235: Mon 29 Jun 2026 09:25:52 WAT */
+/* June 29 Polish Pass 241: Mon 29 Jun 2026 09:25:52 WAT */
