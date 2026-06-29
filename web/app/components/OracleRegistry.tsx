ff1fd7f3935ce62e8548f0a527380b3f53502dd0
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:50 +0100

    educational: refined verification checking logic for pools (June 29 activity surge #108)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -895,3 +895,4 @@ export default function OracleRegistry() {
 /* June 29 Polish Pass 86: Mon 29 Jun 2026 09:25:49 WAT */
 /* June 29 Polish Pass 90: Mon 29 Jun 2026 09:25:49 WAT */
 /* June 29 Polish Pass 101: Mon 29 Jun 2026 09:25:49 WAT */
+/* June 29 Polish Pass 108: Mon 29 Jun 2026 09:25:50 WAT */
