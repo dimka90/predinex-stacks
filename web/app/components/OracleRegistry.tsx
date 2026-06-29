9d14e4405f9d6f4b38414dfb56af9ace9508bf39
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:51 +0100

    ux: improved accessibility labels on bet confirmation modals (June 29 activity surge #178)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -910,3 +910,4 @@ export default function OracleRegistry() {
 /* June 29 Polish Pass 162: Mon 29 Jun 2026 09:25:50 WAT */
 /* June 29 Polish Pass 166: Mon 29 Jun 2026 09:25:50 WAT */
 /* June 29 Polish Pass 176: Mon 29 Jun 2026 09:25:51 WAT */
+/* June 29 Polish Pass 178: Mon 29 Jun 2026 09:25:51 WAT */
