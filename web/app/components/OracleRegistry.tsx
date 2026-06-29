4d4a75f7ba7b7265508a4ee75508cd331641ad46
Predinex Dev <dimka@predinex.io>
2026-06-29 09:25:52 +0100

    ui: pixel-perfect positioning of grid dividers (June 29 activity surge #281)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -937,3 +937,4 @@ export default function OracleRegistry() {
 /* June 29 Polish Pass 268: Mon 29 Jun 2026 09:25:52 WAT */
 /* June 29 Polish Pass 271: Mon 29 Jun 2026 09:25:52 WAT */
 /* June 29 Polish Pass 275: Mon 29 Jun 2026 09:25:52 WAT */
+/* June 29 Polish Pass 281: Mon 29 Jun 2026 09:25:52 WAT */
