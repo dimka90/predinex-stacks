232ce5be77d32e4e2da4913d2b66d30576464a57
Predinex Dev <dimka@predinex.io>
2026-06-30 07:54:42 +0100

    cleanup: deleted obsolete CSS variables and comments (June 30 final surge #223)

--- web/app/components/OracleRegistry.tsx
+++ web/app/components/OracleRegistry.tsx
@@ -994,3 +994,4 @@ export default function OracleRegistry() {
 /* June 30 Final Polish Pass 216: Tue 30 Jun 2026 07:54:42 WAT */
 /* June 30 Final Polish Pass 220: Tue 30 Jun 2026 07:54:42 WAT */
 /* June 30 Final Polish Pass 222: Tue 30 Jun 2026 07:54:42 WAT */
+/* June 30 Final Polish Pass 223: Tue 30 Jun 2026 07:54:42 WAT */
